0077d2ed4069923aa4938d932da23d633b2f4e9a
ArtKit Fityan Aula <fityan@artkit.art>
2024-01-16 20:44:59 +0700

    initial commit

---
+++ examples/src/config.js
@@ -0,0 +1,37 @@
+export default {
+  client_id: "8e648326-8856-4a83-9a27-c4c13bf4abd6",
+  graphScopesOptions: [
+    {
+      key: "user.read",
+      value: "user.read",
+      text: "user.read"
+    },
+    {
+      key: "calendars.read",
+      value: "calendars.read",
+      text: "calendars.read"
+    }
+  ],
+  themeOptions: [
+    {
+      key: "dark",
+      value: "dark",
+      text: "dark"
+    },
+    {
+      key: "light",
+      value: "light",
+      text: "light"
+    },
+    {
+      key: "dark_short",
+      value: "dark_short",
+      text: "dark_short"
+    },
+    {
+      key: "light_short",
+      value: "light_short",
+      text: "light_short"
+    }
+  ]
+};
